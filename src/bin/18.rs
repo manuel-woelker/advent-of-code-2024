2caa8b61cf321c4ab251c9f5ec65ea06028d065d
Manuel Woelker <github@manuel.woelker.org>
2024-12-18 07:06:39 +0100

    day 18

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::{BinaryHeap, HashSet, VecDeque};
 use advent_of_code::map::{Map, Scalar};
 
 advent_of_code::solution!(18);
@@ -13,12 +13,13 @@ enum TileKind {
 struct Tile {
     kind: TileKind,
     is_shortest_path: bool,
+    score: u64,
 }
 
 
 impl Default for Tile {
     fn default() -> Self {
-        Tile { kind: TileKind::Free, is_shortest_path: false }
+        Tile { kind: TileKind::Free, is_shortest_path: false, score: u64::MAX }
     }
 }
 
@@ -39,7 +40,69 @@ struct Path {
     pos: (Scalar, Scalar),
 }
 
+#[derive(PartialEq, Eq)]
+struct Entry {
+    pos: (Scalar, Scalar),
+    distance: u64,
+    score: u64,
+}
+
+
+impl Ord for Entry {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.score.cmp(&self.score)
+    }
+}
+
+impl PartialOrd for Entry {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
+    let mut lines = input.lines();
+    let size: usize = lines.next().unwrap().parse::<usize>().unwrap()+1;
+    let bytes: usize = lines.next().unwrap().parse::<usize>().unwrap();
+    let mut map: Map<Tile> = Map::new(size, size);
+    for line in lines.take(bytes) {
+        let (xs, ys) = line.split_once(',').unwrap();
+        let x: Scalar = xs.parse().unwrap();
+        let y: Scalar = ys.parse().unwrap();
+        map[(x, y)].kind = TileKind::Wall;
+    }
+    map.print_map();
+
+    let start = (0 as Scalar, 0 as Scalar);
+    let end = ((size-1) as Scalar, (size-1) as Scalar);
+    let mut heap = BinaryHeap::new();
+    heap.push(Entry { pos: start, distance: 0, score: 0 });
+    while let Some(entry) = heap.pop() {
+        if entry.pos == end {
+            return Some(entry.score as u32);
+        }
+        let (x, y) = entry.pos;
+        let distance = entry.distance + 1;
+        for (dx, dy) in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
+            let nx = x + dx;
+            let ny = y + dy;
+            if !map.is_in_bounds(nx, ny) || map[(nx, ny)].kind == TileKind::Wall {
+                continue;
+            }
+            let score = distance + (end.0 - nx + end.1 - ny) as u64;
+            if map[(nx, ny)].score > score {
+                map[(nx, ny)].score = score;
+                heap.push(Entry { pos: (nx, ny), distance, score });
+            }
+        }
+
+    }
+
+    None
+}
+
+
+pub fn part_one_bfs(input: &str) -> Option<u32> {
     let mut lines = input.lines();
     let size: usize = lines.next().unwrap().parse::<usize>().unwrap()+1;
     let bytes: usize = lines.next().unwrap().parse::<usize>().unwrap();
@@ -85,13 +148,67 @@ pub fn part_one(input: &str) -> Option<u32> {
     None
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<String> {
+    let mut lines = input.lines();
+    let size: usize = lines.next().unwrap().parse::<usize>().unwrap()+1;
+    let min_bytes: usize = lines.next().unwrap().parse::<usize>().unwrap();
+    let original_map: Map<Tile> = Map::new(size, size);
+    let mut bytes = vec![];
+    for line in lines {
+        let (xs, ys) = line.split_once(',').unwrap();
+        let x: Scalar = xs.parse().unwrap();
+        let y: Scalar = ys.parse().unwrap();
+        bytes.push((x, y));
+    }
+    let len = bytes.len();
+    let mut failed_length = 0;
+    'outer: for i in min_bytes..len {
+//        dbg!(i);
+        let mut map = original_map.clone();
+        for (x, y) in &bytes[0..i] {
+            map[(*x, *y)].kind = TileKind::Wall;
+        }
+
+        let start = (0 as Scalar, 0 as Scalar);
+        let end = ((size - 1) as Scalar, (size - 1) as Scalar);
+        let mut heap = BinaryHeap::new();
+        heap.push(Entry { pos: start, distance: 0, score: 0 });
+        while let Some(entry) = heap.pop() {
+            if entry.pos == end {
+                continue 'outer;
+            }
+            let (x, y) = entry.pos;
+            let distance = entry.distance + 1;
+            for (dx, dy) in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
+                let nx = x + dx;
+                let ny = y + dy;
+                if !map.is_in_bounds(nx, ny) || map[(nx, ny)].kind == TileKind::Wall {
+                    continue;
+                }
+                let score = distance + (end.0 - nx + end.1 - ny) as u64;
+                if map[(nx, ny)].score > score {
+                    map[(nx, ny)].score = score;
+                    heap.push(Entry { pos: (nx, ny), distance, score });
+                }
+            }
+
+        }
+        failed_length = i;
+        break;
+    }
+    let failed_byte = bytes[failed_length-1];
+    Some(format!("{},{}", failed_byte.0, failed_byte.1))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_part_one_bfs() {
+        let result = part_one_bfs(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(22));
+    }
 
     #[test]
     fn test_part_one() {
@@ -102,6 +219,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some("6,1".to_string()));
     }
 }
